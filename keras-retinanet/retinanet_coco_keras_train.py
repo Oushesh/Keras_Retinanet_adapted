a6afb062457f7e9d64c21f1e008e2bc27fd98e1d
oushesh007 <oushesh@concular.com>
2022-07-26 16:31:23 +0200

    updated the code of keras-retinanet

--- keras-retinanet/retinanet_coco_keras_train.py
+++ keras-retinanet/retinanet_coco_keras_train.py
@@ -1,6 +1,3 @@
 """
 python keras_retinanet/bin/train.py --freeze-backbone --random-transform --weights keras_retinanet/snapshots/resnet50_coco_best_v2.1.0.h5 --batch-size 8 --steps 500 --epochs 1 csv keras_retinanet/preprocessing/annotations_bathtub.csv keras_retinanet/preprocessing/classes_bathroom_corrected.csv
-
-Next step: deploy ec2 instance and run the training on it.
-conda activate zind 
 """
